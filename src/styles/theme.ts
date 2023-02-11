2409235bd2104601c474f3e4a1e48d29406076c5
lucastatagiba <marlucetata@hotmail.com>
2023-02-11 13:01:07 -0300

    refact: removing colors pallet

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,37 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
 
-export const colors = {
-  white: {
-    pure: '#FFFFFF',
-    '040': 'rgba(255,255,255,0.4)',
-    '090': 'rgba(255,255,255,0.9)',
-  },
-  black: {
-    pure: '#000000',
-    '050': 'rgba(0, 0, 0, 0.05)',
-    900: '#222222',
-    800: '#292929',
-    700: '#333333',
-    500: '#444444',
-  },
-  gray: {
-    800: '#666666',
-    700: '#8B8B8B',
-    600: '#B6B6B6',
-    500: '#718096',
-    400: '#B4B4B4',
-    300: '#CCCCCC',
-    200: '#EFEFEF',
-    100: '#F2F2F2',
-  },
-  yellow: {
-    100: '#FFF9D8',
-  },
-  boxShadow: {
-    100: 'var(--chakra-shadows-outline)',
-  },
-} as const;
-
 export const theme = extendTheme({
   config: {
     initialColorMode: 'light',
@@ -40,12 +8,11 @@ export const theme = extendTheme({
   fonts: {
     body: 'Roboto, sans-serif',
   },
-  colors,
 
   styles: {
     global: {
       body: {
-        bg: colors.gray['300'],
+        bg: 'lightgray',
       },
     },
   },
